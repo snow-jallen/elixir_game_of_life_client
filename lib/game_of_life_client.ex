ba62c4bf34f5e2a3a17f236eb45cafaef8d9e1fa
Jonathan Allen <jallen@snow.edu>
2020-01-27 13:32:17 -0700

    call /update with simple loop

--- lib/game_of_life_client.ex
+++ lib/game_of_life_client.ex
@@ -8,21 +8,26 @@ defmodule GameOfLifeClient do
   """
   alias HTTPoison
 
+  @endpoint "http://localhost"
+
   def start do
-    {:ok, guid} = register("http://localhost", "test")
-    guid
+    {:ok, guid} = register(@endpoint, "test")
+    IO.inspect guid
+    heartbeat_pid = spawn(fn -> heartbeat(@endpoint, guid) end)
+    heartbeat_pid
   end
 
   def register(endpoint, name) do
     HTTPoison.start
     body = Poison.encode!(%{name: name})
-    headers = [{"Content-type", "application/json"}]
+    headers = [{"content-type", "application/json"}]
     case HTTPoison.post!(endpoint<>"/register", body, headers) do
       %HTTPoison.Response{status_code: 500} ->
         register(endpoint, name <> "_1")
       %HTTPoison.Response{status_code: 200, body: body} ->
         body
         |> Poison.decode
+        |> IO.inspect(label: "register")
         |> case do
           {:ok, %{"token" => token}} -> {:ok, token}
           _ -> {:error, "Unable to identify token in 200 response"}
@@ -30,5 +35,24 @@ defmodule GameOfLifeClient do
       _ ->
         {:error, "Unable to parse response"}
     end
+  end
+
+  def heartbeat(endpoint, token) do
+    Process.sleep(1_000)
+
+    body = Poison.encode!(%{token: token})
+    headers = [{"content-type", "application/json"}]
+    case HTTPoison.post!(endpoint<>"/update", body, headers) do
+      %HTTPoison.Response{status_code: 200, body: body} ->
+        body
+        |> Poison.decode
+        |> IO.inspect(label: "update")
+        |> case do
+          {:ok, %{"isError" => false}} -> "it worked!"
+          {:ok, %{"errorMessage" => error}} -> {:error, error}
+        end
+    end
+
+    heartbeat(endpoint, token)
   end
 end
